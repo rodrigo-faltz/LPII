4da880add90b41d4a4f7d4d4a00b0db5b29e7649
Rodrigo Faltz <rodrigo.faltz@outlook.com>
2025-04-28 18:10:32 -0300

    feat(project): API test e Python script
    Teste de API utilizando o front porém não está funcionando por conta do index.html
    
    Criei um run_servers.py para rodar os dois servidores

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,8 +1,19 @@
+// App.jsx
+import TestAPI from './TestAPI';
+
 function App() {
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <TestAPI />
+      <h1>Welcome to the API Tester</h1>
+      <p>This is a simple application to test API endpoints.</p>
+      <p>Click the buttons above to test the GET and POST endpoints.</p>
+      <p>Check the console for any errors.</p>
+      <p>Make sure your backend server is running on port 3000.</p>
+      <p>Enjoy testing!</p>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
