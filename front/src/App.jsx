20c4d329ea12d8a7713e2ec2ff5325102397fad9
acramin <132026903+acramin@users.noreply.github.com>
2025-04-28 22:05:27 -0300

    feat(router): adiciona navegação com BrowserRouter

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,8 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/criar-conta" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
