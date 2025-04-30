a7818b0832e58de6f5eafab586a14fbd0aad4a20
acramin <132026903+acramin@users.noreply.github.com>
2025-04-29 23:36:33 -0300

    feat(routes): adiciona rota para página de dashboard

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/criar-conta" element={<Register />} />
+        <Route path="/home" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
