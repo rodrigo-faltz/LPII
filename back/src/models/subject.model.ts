a2cbe925da19d0d5a29e68835f08c26000f4fb4d
Rodrigo Faltz <rodrigo.faltz@outlook.com>
2025-04-29 23:31:06 -0300

    add(backend): Add link de imagem
    link_image é enviado no json para ser consumido pelo front

--- back/src/models/subject.model.ts
+++ back/src/models/subject.model.ts
@@ -2,16 +2,20 @@ export interface Subject {
     id: number;
     name: string;
     description: string;
+    image_link: string;
 }
 
 export interface SubjectCreateDTO {
     name: string;
     description: string;
+    image_link?: string;  
 }
 
 export interface SubjectUpdateDTO {
+    id: number;
     name?: string;
     description?: string;
+    image_link?: string;
 }
 
 export interface SubjectDeleteDTO {
