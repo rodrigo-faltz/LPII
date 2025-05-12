0106009b92a0d2958294662f2148f086c41cb138
acramin <132026903+acramin@users.noreply.github.com>
2025-05-12 17:18:45 -0300

    feat(routes): adiciona proteção de rotas privadas com verificação de autenticação

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider, useAuth } from "./services/AuthContext";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -7,17 +8,46 @@ import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import History from "./pages/History";
 
+const PrivateRoute = ({ children }) => {
+  const { authenticated } = useAuth();
+  return authenticated ? children : <Navigate to="/login" />;
+};
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/criar-conta" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/home/explorar" element={<Explore />} />
-        <Route path="/home/historico" element={<History />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/criar-conta" element={<Register />} />
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/home/explorar"
+            element={
+              <PrivateRoute>
+                <Explore />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/home/historico"
+            element={
+              <PrivateRoute>
+                <History />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
