7095cf4e55ea3249a3e3d3392e45c14f7f00bc4d
acramin <132026903+acramin@users.noreply.github.com>
2025-05-17 17:52:30 -0300

    refactor(app): altera rota do chat de /chat para /home/chat

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -48,7 +48,7 @@ function App() {
             }
           />
           <Route
-            path="/chat"
+            path="home/chat"
             element={
               <PrivateRoute>
                 <Chat />
