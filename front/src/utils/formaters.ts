1c9f4b38a0b3cd712cdcdb324b7e72d2f22cc341
acramin <132026903+acramin@users.noreply.github.com>
2025-05-17 18:05:21 -0300

    feat(history): adiciona formatação de data e hora na listagem de chats

---
+++ front/src/utils/formaters.ts
@@ -0,0 +1,9 @@
+export const formatDateTime = (dateString: string): string => {
+  return new Intl.DateTimeFormat("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(new Date(dateString));
+};
