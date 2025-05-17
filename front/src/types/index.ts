eae5f3d64786ebf1fe8f1a0b6923e5b4be8df611
acramin <132026903+acramin@users.noreply.github.com>
2025-05-17 18:06:13 -0300

    feat(types): cria type Chat para padronizar dados exibidos na listagem de chats

---
+++ front/src/types/index.ts
@@ -0,0 +1,12 @@
+export type Chat = {
+  id: string;
+  titulo: string;
+  materia: string;
+  dataHora: string;
+  ultimaMensagem: string;
+};
+
+export type FilterOption = {
+  value: string;
+  label: string;
+};
