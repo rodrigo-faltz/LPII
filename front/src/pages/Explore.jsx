97f796f77848baa81c34f8b053b0b08723e19bdc
acramin <132026903+acramin@users.noreply.github.com>
2025-05-24 09:10:32 -0300

    fix(explore): corrige scroll

--- front/src/pages/Explore.jsx
+++ front/src/pages/Explore.jsx
@@ -48,19 +48,18 @@ class Explore extends React.Component {
   }
 
   render() {
-    
     return (
-      <div className="container-fluid p-0">
-        <div className="row g-0">
+      <div className="container-fluid p-0 vh-100">
+        <div className="row g-0 h-100">
           <Sidebar
             activeNav={this.state.activeNav}
             setActiveNav={(newNav) => this.setState({ activeNav: newNav })}
           />
 
-          <div className="col main-content">
+          <div className="col main-content d-flex flex-column h-100">
             <Header />
 
-            <div className="container-fluid p-4">
+            <div className="container-fluid p-4 flex-grow-1 overflow-auto">
               <SubjectsSection
                 title="Todas as disciplinas"
                 subjects={this.allSubjects}
