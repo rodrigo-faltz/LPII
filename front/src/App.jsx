5c694ba2505a124eca20a3893d9041ce8975a92c
acramin <132026903+acramin@users.noreply.github.com>
2025-05-25 15:20:21 -0300

    fix(router): remove prop `component` e usa apenas `element` para rota de chat por ID

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,7 +5,7 @@ import { Navigate } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Home from "./pages/Home";
+//import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import History from "./pages/History";
 import Chat from "./pages/Chat";
@@ -57,7 +57,6 @@ function App() {
           />
           <Route
             path="/home/chat/:id"
-            component ={Home}
             element={
               <PrivateRoute>
                 <Chat />
